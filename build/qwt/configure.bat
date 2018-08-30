451ca7770cfc166d546bd3c33c3406529e854562
andry81 <andry@inbox.ru>
2018-08-30 17:33:22 +0000

    * typo
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/3dparty/trunk@6 59fabf34-1de7-47f8-88d4-23c2723bc666

--- build/qwt/configure.bat
+++ build/qwt/configure.bat
@@ -6,7 +6,7 @@
   echo.rem set QT_ROOT=c:\Qt\Qt5.6.0_vc12\5.6\msvc2013
   echo.set QT_ROOT=c:\Qt\Qt5.7.1_vc14_x86\5.7\msvc2015
   echo.set SRC_BASE_DIR=qwt
-  echo.set SRC_DIR=qwt-6.1.3
+  echo.set SRC_DIR=qwt-6.1.3-src
   echo.set TOOLSET=msvc-14.0
   echo.set ADDRESS_MODEL=32
   echo.set "WINDOWS_KIT_BIN_ROOT=c:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0"
