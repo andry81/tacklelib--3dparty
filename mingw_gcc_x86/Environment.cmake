1135e1fe0613e70aa68b82eb6f6f2b6a960e487a
andry81 <andry@inbox.ru>
2019-01-20 15:47:43 +0000

    * fixed: boost correct detection for version 1.66 and higher (https://gitlab.kitware.com/cmake/cmake/merge_requests/2568)
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/3dparty/trunk@10 59fabf34-1de7-47f8-88d4-23c2723bc666

--- mingw_gcc_x86/Environment.cmake
+++ mingw_gcc_x86/Environment.cmake
@@ -1,5 +1,6 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../common/PwEnvironment.cmake)
 
 SET(ENV{BOOST_ROOT} ${CMAKE_CURRENT_LIST_DIR}/boost/boost_1_68_0)
+set(ENV{Boost_ARCHITECTURE} "-x32") # required for boost 1.66 and higher: https://gitlab.kitware.com/cmake/cmake/merge_requests/2568
 
 set(ENV{QWT_ROOT} ${CMAKE_CURRENT_LIST_DIR}/qwt/qwt-6.1.3)
